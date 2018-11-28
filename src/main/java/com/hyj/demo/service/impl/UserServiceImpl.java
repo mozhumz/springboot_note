3588cd55261f75de44597152d704762c2a2ff548
mozhumz <554501252@qq.com>
2018-11-28 10:59:53 +0800

    feat:add test yaml

--- src/main/java/com/hyj/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/hyj/demo/service/impl/UserServiceImpl.java
@@ -22,106 +22,106 @@ import net.sf.json.JSONArray;
 
 @Service
 public class UserServiceImpl implements IUserService {
-  @Resource
-  IUserMapper userMapper;
+	@Resource
+	IUserMapper userMapper;
 
-  @Resource
-  IMysqlMapper mysqlMapper;
+	@Resource
+	IMysqlMapper mysqlMapper;
 
-  @Resource
-  IUserRepositry userRepositry;
+	@Resource
+	IUserRepositry userRepositry;
 
-  @Override
-  public List<UserPO> getList() {
-    // TODO Auto-generated method stub
-    List<UserPO>list=userMapper.getUserList();
-//    JSONArray arr =new JSONArray(list.get(0).getArr());
-//    List<Map<String,Object>>arr =(List<Map<String,Object>>)list.get(0).getArr();
-    for(UserPO user : list) {
-        
-      JSONArray  arr=net.sf.json.JSONArray.fromObject(user.getArr());
-      List arrlist =new ArrayList<>();
-      for(int i=0;i<arr.size();i++) {
-        arrlist.add(arr.getJSONObject(i));
-      }
-      
-//      user.setArrList(arrlist);
-//      for(JSONObject obj : arr.iterator().) {
-//        
-//      }
-      System.out.println("list: "+list);
-    }
-    return list;
-  }
+	@Override
+	public List<UserPO> getList() {
+		// TODO Auto-generated method stub
+		List<UserPO> list = userMapper.getUserList();
+		// JSONArray arr =new JSONArray(list.get(0).getArr());
+		// List<Map<String,Object>>arr =(List<Map<String,Object>>)list.get(0).getArr();
+		for (UserPO user : list) {
 
-  @Override
-  public int addUserPO(Map<String, Object> params) {
-    // TODO Auto-generated method stub
-    try {
-      System.out.println(params);
-//      String arr=params.get("arr")+"";
-      params.put("arr", com.alibaba.fastjson.JSONObject.toJSONString(params.get("arr")));
-      System.out.println(userMapper.addUserPO(params));
-    } catch (Exception e) {
-      e.printStackTrace();
-      return -1;
-    }
-    return 0;
-  }
+			JSONArray arr = net.sf.json.JSONArray.fromObject(user.getArr());
+			List arrlist = new ArrayList<>();
+			for (int i = 0; i < arr.size(); i++) {
+				arrlist.add(arr.getJSONObject(i));
+			}
 
-  @Override
-  public int addUserPOList() {
-    try {
-      List<UserPO>list=new ArrayList<>();
-      for(int i=0;i<3;i++) {
-        UserPO po =new UserPO("name"+i,""+i,"arr"+i);
-        list.add(po);
-      }
-      int lines=userMapper.insertList(list);
-      System.out.println(lines);
-      
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return 0;
-  }
-
-  @Override
-  @Scheduled(initialDelay=1000*100,fixedRate=1000*100)
-  public void testSchedule() {
-    // TODO Auto-generated method stub
-    System.out.println("service testSchedule ok");
-    
-  }
-
-  @Override
-  public Map getAllMysqlUser() {
-    Map<String,Object>res=new HashMap<>();
-    List list=mysqlMapper.findAllUser();
-    res.put("data",list);
-    return res;
-  }
-
-  @Override
-  public UserPO getOneUser() {
-    UserPO po=new UserPO();
-//    System.out.println(userRepositry.findOne(1));
-    return  po;
-  }
-
-@Override
-public void saveUser(UserPO userPO) {
-	// TODO Auto-generated method stub
-	try {
-		
-		userRepositry.save(userPO);
-	} catch (DataIntegrityViolationException e) {
-		// TODO: handle exception
-		e.printStackTrace();
-		System.out.println("----------------------------------");
-		System.out.println(e.getMessage());
+			// user.setArrList(arrlist);
+			// for(JSONObject obj : arr.iterator().) {
+			//
+			// }
+			System.out.println("list: " + list);
+		}
+		return list;
 	}
-	System.out.println(userPO);
-}
+
+	@Override
+	public int addUserPO(Map<String, Object> params) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println(params);
+			// String arr=params.get("arr")+"";
+			params.put("arr", com.alibaba.fastjson.JSONObject.toJSONString(params.get("arr")));
+			System.out.println(userMapper.addUserPO(params));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+		return 0;
+	}
+
+	@Override
+	public int addUserPOList() {
+		try {
+			List<UserPO> list = new ArrayList<>();
+			for (int i = 0; i < 3; i++) {
+				UserPO po = new UserPO("name" + i, "" + i, "arr" + i);
+				list.add(po);
+			}
+			int lines = userMapper.insertList(list);
+			System.out.println(lines);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@Override
+	@Scheduled(initialDelay = 1000 * 100, fixedRate = 1000 * 100)
+	public void testSchedule() {
+		// TODO Auto-generated method stub
+		System.out.println("service testSchedule ok");
+
+	}
+
+	@Override
+	public Map getAllMysqlUser() {
+		Map<String, Object> res = new HashMap<>();
+		List list = mysqlMapper.findAllUser();
+		res.put("data", list);
+		return res;
+	}
+
+	@Override
+	public UserPO getOneUser() {
+		UserPO po = new UserPO();
+		// System.out.println(userRepositry.findOne(1));
+		return po;
+	}
+
+	@Override
+	public void saveUser(UserPO userPO) {
+		// TODO Auto-generated method stub
+		try {
+
+			userRepositry.save(userPO);
+		} catch (DataIntegrityViolationException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("----------------------------------");
+			System.out.println(e.getMessage());
+		}
+		System.out.println(userPO);
+	}
 
 }
