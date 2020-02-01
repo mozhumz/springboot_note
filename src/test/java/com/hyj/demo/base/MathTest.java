9aab8b1aa0c4c1b75b5dd3f52bbd16c44e364a25
mozhumz <Mf.731210>
2020-02-01 16:06:51 +0800

    全排列

---
+++ src/test/java/com/hyj/demo/base/MathTest.java
@@ -0,0 +1,44 @@
+package com.hyj.demo.base;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MathTest {
+
+    /**
+     * 全排列算法
+     */
+    @Test
+    public void test1(){
+        List<Integer> s = new ArrayList<Integer>();
+        List<Integer> rs = new ArrayList<Integer>();
+        for (int i = 1; i <= 5; i++)
+            s.add(i);
+        pl(s, rs);
+    }
+
+    /**
+     * 全排列算法
+     * @param s
+     * @param rs
+     */
+    public static void pl(List<Integer> s, List<Integer> rs) {
+        if (s.size() == 1) {
+            rs.add(s.get(0));
+            System.out.println(rs.toString());
+            rs.remove(rs.size() - 1);
+        } else {
+            for (int i = 0; i < s.size(); i++) {
+                rs.add(s.get(i));
+                List<Integer> tmp = new ArrayList<Integer>();
+                for (Integer a : s)
+                    tmp.add(a);
+                tmp.remove(i);
+                pl(tmp, rs);
+                rs.remove(rs.size() - 1);
+            }
+        }
+    }
+}
